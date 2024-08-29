8cdbb34780a8786cd6f5a087b27ebc9f004cbb92
KaspianDev <kacperleaguenine@gmail.com>
2024-08-29 21:57:14 +0200

    Add POBoxReward

--- src/main/java/com/github/kaspiandev/fishybusiness/hook/POBoxHook.java
+++ src/main/java/com/github/kaspiandev/fishybusiness/hook/POBoxHook.java
@@ -2,15 +2,15 @@ package com.github.kaspiandev.fishybusiness.hook;
 
 import com.github.kaspiandev.fishybusiness.FishyBusiness;
 import com.github.kaspiandev.fishybusiness.config.Message;
-import com.github.kaspiandev.fishybusiness.reward.ItemReward;
-import com.github.kaspiandev.fishybusiness.reward.RewardType;
-import com.github.kaspiandev.fishybusiness.reward.RewardTypeRegistry;
+import com.github.kaspiandev.fishybusiness.reward.*;
 import com.github.kaspiandev.fishybusiness.reward.adapter.ItemB64Adapter;
 import com.github.kaspiandev.pobox.POBox;
 import com.github.kaspiandev.pobox.event.MailClaimEvent;
 import com.github.kaspiandev.pobox.mail.ItemMail;
+import com.github.kaspiandev.pobox.mail.Mail;
 import com.github.kaspiandev.pobox.mail.MailManager;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -28,6 +28,7 @@ public class POBoxHook extends Hook<POBox> implements Listener {
     protected void load() {
         ItemB64Adapter itemAdapter = new ItemB64Adapter();
         RewardTypeRegistry.register("item", new RewardType(ItemReward.class, itemAdapter));
+        RewardTypeRegistry.register("pobox", new RewardType(POBoxReward.class));
     }
 
     public void stashItem(Player player, ItemStack item) {
@@ -52,6 +53,35 @@ public class POBoxHook extends Hook<POBox> implements Listener {
 
         player.spigot().sendMessage(plugin.getMessages().get(Message.MAIL_CANNOT_CLAIM, player));
         event.setCancelled(true);
+    }
+
+    public RewardMail buildRewardMail(String name, Reward reward) {
+        return new RewardMail(name, reward);
+    }
+
+    public RewardMail buildRewardMail(String name, Reward reward, Material icon) {
+        return new RewardMail(name, reward, icon);
+    }
+
+    public class RewardMail extends Mail {
+
+        private final String rewardName;
+
+        public RewardMail(String name, Reward reward) {
+            super(name);
+            this.rewardName = reward.getName();
+        }
+
+        public RewardMail(String name, Reward reward, Material icon) {
+            super(name, icon);
+            this.rewardName = reward.getName();
+        }
+
+        @Override
+        public void claim(Player player) {
+            plugin.getRewardManager().findReward(rewardName).ifPresent((reward) -> reward.reward(plugin, player));
+        }
+
     }
 
 }
---
+++ src/main/java/com/github/kaspiandev/fishybusiness/reward/POBoxReward.java
@@ -0,0 +1,42 @@
+package com.github.kaspiandev.fishybusiness.reward;
+
+import com.github.kaspiandev.fishybusiness.FishyBusiness;
+import com.github.kaspiandev.fishybusiness.hook.POBoxHook;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class POBoxReward implements Reward {
+
+    private final String name;
+    private final Reward reward;
+    private final double weight;
+
+    public POBoxReward(String name, Reward reward, double weight) {
+        this.name = name;
+        this.reward = reward;
+        this.weight = weight;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public double getWeight() {
+        return weight;
+    }
+
+    @Override
+    public void reward(FishyBusiness plugin, Player player) {
+        plugin.getHookManager().findHook(POBoxHook.class).ifPresent((hook) -> {
+            hook.buildRewardMail(name, reward);
+        });
+    }
+
+    @Override
+    public ItemStack getDisplay(FishyBusiness plugin, Player player) {
+        return reward.getDisplay(plugin, player);
+    }
+
+}
